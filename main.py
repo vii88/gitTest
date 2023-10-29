e016b94da3e644ac815cb82489db0a1b14149c82
Admin <vitya2007.vas@gmail.com>
2023-10-29 10:28:41 +0200

    Add input name

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from src.hello import func
-
-def print_hi(name):
+print("Here")
+def print_hi():
     # Use a breakpoint in the code line below to debug your script.
-    print(f' {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi {ask()}')  # Press Ctrl+F8 to toggle the breakpoint.
+def ask():
+    return input('ваше имя: ')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi(func())
+    print_hi()
+
+#some text///////////////////
