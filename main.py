92ed8f1bacfbaca78a5d1837a66d44c92e943c41
Admin <vitya2007.vas@gmail.com>
2023-11-12 09:13:21 +0200

    Added func func12() in main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from src.hello import func
+from src.hello import func,func12
 print("Here")
 def print_hi():
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi {ask()}')  # Press Ctrl+F8 to toggle the breakpoint.
 def ask():
-    print("bye")
+    print(func12())
     return input('ваше имя: ')
 
 # Press the green button in the gutter to run the script.
