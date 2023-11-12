fbd2f1b4a282d10392fb13baf0b457891a230af8
Admin <vitya2007.vas@gmail.com>
2023-11-12 09:49:35 +0200

    Added func() function

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from src.hello import func, func12, new_func
 
-print("Here")
-
-
 def print_hi():
-    print(f'Hi,today num is" {new_func()}\n {ask()}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'hello,today num is" {new_func()}\n {ask()}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 def ask():
-    print(func12())
+    print(f'{func()} {func12()}')
     return input('ваше имя: ')
 
 
