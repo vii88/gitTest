59ac13994c0ddf9eceb8351189797d18ea8ad804
Admin <vitya2007.vas@gmail.com>
2023-11-12 09:09:17 +0200

    Create new_func() and added in main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from src.hello import func
+from src.hello import func,new_func
 print("Here")
 def print_hi():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi {ask()}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi,today num is" {new_func()}\n {ask()}')  # Press Ctrl+F8 to toggle the breakpoint.
 def ask():
     print("bye")
     return input('ваше имя: ')
